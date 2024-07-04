8421800066d9f4ad59a53d7d105d72187b47014b
AnthonyR <anthony156gamer@gmail.com>
2024-07-04 11:13:31 +0200

    :sparkles: Add controller

---
+++ src/main/java/com/projects/springspellchecker/controller/SpringSpellCheckerController.java
@@ -0,0 +1,7 @@
+package com.projects.springspellchecker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSpellCheckerController {
+}
