1334280bb175c32237b189a0f223e5256bb6b76d
Antho <anthony.rolland@proton.me>
2024-07-04 10:44:48 +0200

    :tada: initialise le projet

---
+++ src/main/java/com/projects/orthocheck/OrthoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.projects.orthocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrthoCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrthoCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/orthocheck/OrthoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.orthocheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrthoCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
